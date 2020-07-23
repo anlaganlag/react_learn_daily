0277ea289b20776b412fee24f830ec6f6a63554e
anlaganlag <tanjhu@163.com>
2020-07-23 17:20:21 +0800

    useReducer计时器

--- 0722/src/App.js
+++ 0722/src/App.js
@@ -1,31 +1,25 @@
 import React, { useState  , useContext,createContext, useReducer } from 'react';
  
 
-
+function reducer(state,action){
+  switch (action.type){
+    case "increment":
+      return state+1;
+    case "decrement":
+      return state -1;
+    default:
+      return state;
+  }
+}
 
 function App(){
-    const [count, setCount] = useState(0)
-    const [firstName, setFirstName] = useState("")
-    const handleReSet=()=>{
-      setCount(0)
-      setFirstName("")
-    }
-
-
-    return (
-        <div className="App">
-          <button onClick={()=> setCount(c=>c+1)}>+</button>
-          <div>count:{count}</div>
-
-          <input
-            value={firstName}
-            onChange={e=>{
-              setFirstName(e.target.value)
-              setCount(e=>e+1)
-            }}
-          />
-          <button onClick={handleReSet}>Reset Count&Text</button>
-        </div>
-    );
+  const [count, dispatch] = useReducer(reducer, 0)
+  return (
+    <div>
+      <div>count: {count}</div>
+      <button onClick={()=>dispatch({ type:"increment"})}>increment</button>
+      <button onClick={()=>dispatch({ type:"decrement"})}>decrement</button>
+    </div>
+  );
 }
 export default App;
