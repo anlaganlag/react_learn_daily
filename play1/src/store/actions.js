4276f784d71ae9ee2693dc0a4e27d3ca582f1a9b
anlaganlag <tanjhu@163.com>
2020-08-06 13:41:30 +0800

    重構add和remove產品函數

--- play1/src/store/actions.js
+++ play1/src/store/actions.js
@@ -1,24 +1,30 @@
+
 export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
 export const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODCUT_FROM_CART';
 
-export const addProductToCart = product => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch({
-        type: ADD_PRODUCT_TO_CART,
-        payload: product
-      });
-    }, 700);
-  };
-};
 
-export const removeProductFromCart = productId => {
-    return dispatch => {
-      setTimeout(() => {
-        dispatch({
-          type: REMOVE_PRODUCT_FROM_CART,
-          payload: productId
-        });
-      }, 700);
-    };
-  };
+
+export const addProductToCart = product =>
+    dispatch =>
+        setTimeout(
+            () => dispatch({
+                type:ADD_PRODUCT_TO_CART,
+                payload:product
+            }) ,0)
+
+        
+
+
+
+
+
+export const removeProductFromCart = productId =>
+    dispatch =>
+        setTimeout(
+            ()=> dispatch({
+                type:REMOVE_PRODUCT_FROM_CART,
+                payload:productId
+            })
+        ,0)
+    
+
