20dcfe1bc9059a468dcc2cc93460fef010a16be0
anlaganlag <tanjhu@163.com>
2020-08-16 22:57:53 +0800

    flex

--- webease_usehook_最佳实践/src/ThemeContext.js
+++ webease_usehook_最佳实践/src/ThemeContext.js
@@ -12,18 +12,3 @@ export  function useThemeUpdate() {
     return useContext(ThemeUpdateContext)
 }
 
-export function ThemeProvider( { children }) {
-    const [darkTheme,setDarkTheme] =useState(true)
-
-    function toggleTheme(){
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
-
-    return (
-        <ThemeContext.Provider value={darkTheme}>
-            <ThemeUpdateContext.Provider value={toggleTheme}>
-                { children }
-            </ThemeUpdateContext.Provider>
-        </ThemeContext.Provider>
-    )
-}
