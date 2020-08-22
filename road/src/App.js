51677788189a3fbb31474586e4141a1e67e4440b
anlaganlag <tanjhu@163.com>
2020-08-22 10:19:17 +0800

    最簡單的event處理

--- road/src/App.js
+++ road/src/App.js
@@ -1,34 +1,7 @@
-import React, { useState, useContext, createContext } from "react";
+import React,{useState} from "react";
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-  {
-    title: 'KingOfCode',
-    url: 'https://kingofcode.js.org/',
-    author: 'Best Coder',
-    num_comments: 5,
-    points: 10,
-    objectID: 2,
-    },
-]
-
-const List = () =>
-  list.map((item) => (
+const List = props =>
+  props.list.map((item) => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -40,16 +13,46 @@ const List = () =>
   ));
 
 const App = () => {
+  const list = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    {
+      title: 'KingOfCode',
+      url: 'https://kingofcode.js.org/',
+      author: 'Best Coder',
+      num_comments: 5,
+      points: 10,
+      objectID: 2,
+      },
+  ]
+  const [searchTerm, setSearchTerm] = useState('')
   const handleChange = (event) => {
-    console.log(event);
+      setSearchTerm(event.target.value);
   };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+      <p> Searching for <strong>{searchTerm}</strong> </p>
+
       <hr />
-      <List />
+      <List list={list}/>
     </div>
   );
 };
