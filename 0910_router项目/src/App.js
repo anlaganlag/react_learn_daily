4c985e04174be3d0a573636fd1ee856e010b36a8
anlaganlag <tanjhu@163.com>
2020-09-10 12:13:12 +0800

    简单的router项目

--- 0910_router项目/src/App.js
+++ 0910_router项目/src/App.js
@@ -1,29 +1,52 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import { Home, About, Events, Products, Contact } from "./pages";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Home, About, Events, Products, Contact, Whoops404 } from "./pages";
+import "./App.css"
 function App() {
   return (
-    <div>
+    <>
       <Router>
-        <Route exact path="/">
-          <Home />{" "}
-        </Route>
-        <Route path="/about">
-          <About />{" "}
-        </Route>
-        <Route path="/events">
-          <Events />{" "}
-        </Route>
-        <Route path="/products">
-          <Products />{" "}
-        </Route>
-        <Route path="/contact">
-          <Contact />{" "}
-        </Route>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />{" "}
+          </Route>
+          <Route path="/about">
+            <About />{" "}
+          </Route>
+          <Route path="/events">
+            <Events />{" "}
+          </Route>
+          <Route path="/products">
+            <Products />{" "}
+          </Route>
+          <Route path="/contact">
+            <Contact />{" "}
+          </Route>
+          <Route path="*">
+            <Whoops404 />{" "}
+          </Route>
+        </Switch>
       </Router>
-    </div>
+    </>
   );
 }
 export default App;
