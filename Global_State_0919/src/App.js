1042a7a9193059decf5fe8eb18fb8a97b9f49f50
anlaganlag <tanjhu@163.com>
2020-09-19 14:35:22 +0800

    Global_State_with_Context_React

---
+++ Global_State_0919/src/App.js
@@ -0,0 +1,14 @@
+import React, {    } from 'react';
+import CC from './ccp'
+import Store from  './store'
+
+
+
+function App(){
+    return (
+        <Store>
+          <CC />
+        </Store>
+    )
+}
+export default App;
---
+++ Global_State_0919/src/ccp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CC() {
+    return (
+        <div>
+            牛逼的cp
+        </div>
+    )
+}
+
+export default CC
