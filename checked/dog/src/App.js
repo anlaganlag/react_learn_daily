b10dc666d1ada27d433bb09d00dba6c8f049c6ee
anlaganlag <tanjhu@163.com>
2020-11-11 17:37:59 +0800

    選狗項目可以正確的選中

--- checked/dog/src/App.js
+++ checked/dog/src/App.js
@@ -3,47 +3,107 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState("");
+  const [wiki, setWiki] = useState("");
+  const [wiki2, setWiki2] = useState("");
+  const [main, setMain] = useState("")
+  
   const [breed, setBreed] = useState("");
   const [loading, setLoading] = useState(false);
+  const dogURL = "https://dog.ceo/api/breeds/image/random";
+  const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
   function handleFetchDog() {
     console.log("从API获取一只狗的图片");
+    setData("");
     setLoading(true);
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch(dogURL)
       .then((res) => res.json())
       .then((data) => {
         setData(data.message);
         // 即获取到数据等一秒再set,即等载入图片的时间
         // 否则文字出了图片还要等一会
-        setTimeout(()=>{
-          setBreed(data.message.split("/")[4])
-          setLoading(false)
-        },1000)
-        console.log(data);
+        console.log(data.message, "圖片來源");
+
+        const l1 = data.message.split("/")[4].split("-");
+        if (l1.length === 2) {
+          const [a, b] = l1;
+          setBreed(b + "_" + a);
+          setMain(a)
+        } else {
+          setBreed(l1);
+          setMain(l1)
+        }
+        setLoading(false);
       })
+
       .catch((e) => {
         console.error("没有找到狗子", e);
         setLoading(false);
       });
   }
+
+  function handleIntroDog() {
+    console.log("獲取介紹狗狗的卡片");
+    // setData("")
+    fetch(wikiUrl + breed)
+      .then((res) => res.json())
+      .then((wiki) => {
+        setWiki(wiki);
+        console.log(wiki, "jjjjjjjj");
+      });
+  }
+
+  function handleIntroDog2() {
+    console.log(breed,"獲取介紹狗狗主類");
+    if (breed[0]?.split("_").length ===1) return
+    console.log(breed);
+    // setData("")
+    fetch(wikiUrl + breed.split("_")[1])
+      .then((res) => res.json())
+      .then((wiki) => {
+        setWiki2(wiki);
+        console.log(wiki, "豬豬豬豬");
+      });
+  }
+
+  // Alaskan_Malamute
+
   useEffect(() => {
     console.log("调用useEffect");
     handleFetchDog();
   }, []);
 
+  useEffect(() => {
+    console.log("breed變化了", breed);
+    // setTimeout(
+    //   ()=>handleIntroDog(),1000
+
+    // )
+    handleIntroDog();
+    handleIntroDog2()
+  }, [breed]);
+
   return (
     <>
       <h1>随机一狗项目</h1>
-      <button onClick={handleFetchDog}> 换张狗子图片</button>
-      {data && <img src={data} alt="狗子图片" onClick={handleFetchDog} />}
-      {loading 
-        ? <h4>载入中.....</h4> 
-        : <a href={`http://www.baidu.com/s?wd=${breed +" 犬"}`} target={"_blank"}> <h4>品种:{breed}</h4> 
-          </a>
-      }
-
-      </>
-      
+      {!loading && <img src={data} alt="狗子图片" onClick={handleFetchDog} />}
+      {loading ? (
+        <h4>载入中.....</h4>
+      ) : (
+        <a
+          href={`http://www.baidu.com/s?wd=${breed + " 犬"}`}
+          target={"_blank"}
+        >
+          {" "}
+      <h1>主類 {main} </h1>
+{         main !==breed && <h6>亞類 {breed.split("_").join(" ")} 
+          
+          </h6>}
+        </a>
+      )}
+      {/* <button onClick={handleIntroDog}>狗狗維基百科信息</button> */}
+      {wiki.title !== "Not found." && <p>{wiki.extract}</p>}
+    </>
   );
 }
 export default App;
---
+++ 選狗/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [breed, setBreed] = useState("")
+  const [image, setImage] = useState("");
+  const breedsURL = "https://dog.ceo/api/breeds/list";
+  const randomURL = "https://dog.ceo/api/breeds/image/random";
+
+  useEffect(() => {
+    fetch(breedsURL)
+      .then((resp) => resp.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  useEffect(() => {
+    fetch(randomURL)
+      .then((resp) => resp.json())
+      .then((img) => setImage(img.message));
+  }, []);
+
+  console.log(data, "數據");
+  console.log(image, "圖片");
+  return (
+    <>
+      <header>
+        <h1 className="logo">選狗項目</h1>
+      </header>
+      <div className="container">
+        <label htmlFor="breeds">選擇狗品種</label>
+        <select
+          className="u-full-width"
+          value={breed}
+          onChange={e => setBreed(e.target.value)}
+          id="breeds"
+        >
+          {data.map((option) => 
+            <option key={option} value={option} >
+            
+              {option} 
+            </option>
+        )}
+        </select>
+
+{ 
+<>
+<img src={image} alt /> 
+
+<p>Click to view images of {breed}</p>
+</>
+
+}
+
+        <div className="card"></div>
+      </div>
+    </>
+  );
+}
+export default App;
